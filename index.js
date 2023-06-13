c371d4927b01f8302d3e9fd9d94e74124f902c6d
anupcodebubble <codebubble60@gmail.com>
2023-06-13 18:35:28 +0545

    new_update

--- index.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const server = require("./config/server");
-require("./config/database");
-
-const PORT = process.env.PORT || 5000;
-
-server.listen(PORT, () => {
-  console.log(`app running on port ${PORT}`);
-});
